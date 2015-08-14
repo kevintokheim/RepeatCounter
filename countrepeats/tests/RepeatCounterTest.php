4157f0afa574ac26935fef94fd6fec799b4b2ff3
Kevin <katokh89@gmail.com>
2015-08-14 10:37:58 -0700

    add twoWords test and unmodified successful method

--- countrepeats/tests/RepeatCounterTest.php
+++ countrepeats/tests/RepeatCounterTest.php
@@ -18,6 +18,20 @@
             //Assert
             $this->assertEquals("a", $result);
         }
+
+        function test_countRepeats_twoWords()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input_word = "a";
+            $input_string = "a b";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input_word, $input_string);
+
+            //Assert
+            $this->assertEquals("a", $result);
+        }
     }
 
 
