c441eb319b35f652c75883ea3116f12d04e36bec
Kevin <katokh89@gmail.com>
2015-08-14 15:12:56 -0700

    add app and view files

---
+++ countrepeats/app/app.php
@@ -0,0 +1,31 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    //Routes
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('repeatcounter.html.twig');
+    });
+
+    $app->get("/results", function() use ($app) {
+        $new_repeat_counter = new RepeatCounter;
+        $repeat_counter_results = $new_repeat_counter->countRepeats($_GET['input_word'], $_GET['input_string']);
+
+        return $app['twig']->render('results.html.twig', array('result' => $repeat_counter_results));
+    });
+
+    return $app;
+
+ ?>
